089ef5ab782219d677cbd36659da476d9770be1a
dp <davepollan@gmail.com>
2019-12-19 22:15:22 -0500

    final functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+    // Gets the local storage values for all time blocks and sets them if they already exist
     // 9AM
     var t0 = localStorage.getItem("9am");
     $("#nine-am").text(t0);
@@ -42,11 +43,38 @@ $(document).ready(function(){
         var value = $(this).siblings(".description").val(); // This gets the VALUE of the description in jQuery
         var time = $(this).siblings(".hour").text();
         localStorage.setItem(time, value);
-
         $("this").siblings(".description").textContent(localStorage.value);
-        
 
     });
+    // Displays the current date and time
     $("#currentDay").text(moment().format("llll"));
+
+    // Determines which color class to apply based on the current hour
+    var elements = $(".time-block");
+    var d = new Date();
+   var h = d.getHours();
+   // var h = 15;
+    
+    for (i = 0; i < elements.length; i++) {
+        var t = parseInt(elements[i].getAttribute("data-hour"));
+
+        if (h < t) {
+            elements[i].classList.add('future');
+            elements[i].classList.remove('past');
+            elements[i].classList.remove('present');
+        }  else if (h > t) {
+            elements[i].classList.add('past');
+            elements[i].classList.remove('future');
+            elements[i].classList.remove('present');
+        } else if (h === t) {
+            elements[i].classList.add('present');
+            elements[i].classList.remove('future');
+            elements[i].classList.remove('past');
+        } else if (h > 16 && h < 9) {
+            elements[i].classList.add('future');
+            elements[i].classList.remove('past');
+            elements[i].classList.remove('present');
+        }
+    }
 
 });
